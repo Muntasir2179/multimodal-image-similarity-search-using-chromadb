c37d9b40d8c144b76d6d76cbf2c5916382fb659b
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-24 13:27:56 +0600

    user authentication complete

--- image_search/urls.py
+++ image_search/urls.py
@@ -3,4 +3,7 @@ from image_search import views
 
 urlpatterns = [
     path('', view=views.index, name='index'),
+    path('login/', view=views.login_function, name='login'),
+    path('signup/', view=views.signup_function, name='signup'),
+    path('logout/', view=views.logout_function, name='logout')
 ]
--- image_search/views.py
+++ image_search/views.py
@@ -1,6 +1,104 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login as loginUser, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+def login_function(request):
+    if request.method == 'GET':
+        # if authenticated user tries to access the login url again then redirect to home page
+        if request.user.is_authenticated:
+            return redirect('index')
+        form = AuthenticationForm()
+        context = {
+            "form": form
+        }
+        return render(request=request, template_name='login.html', context=context)
+    else:
+        error_message = None
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                loginUser(request, user)
+                return redirect('index')
+        else:
+            username = request.POST.get('username')
+            all_user = User.objects.all()
+            error_message = "No user exit with this username"
+            for single_user in all_user:
+                if username == single_user.username:
+                    error_message = "Password doesn't match"
+
+            context = {
+                "form": form,
+                "error_message": error_message
+            }
+            return render(request=request, template_name='login.html', context=context)
+
+
+def signup_function(request):
+    if request.method == 'GET':
+        # if authenticated user tries to access the signup url again then redirect to home page
+        if request.user.is_authenticated:
+            return redirect('index')
+        form = UserCreationForm()
+        context = {
+            'form': form
+        }
+        return render(request=request, template_name='signup.html', context=context)
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # forming error messages
+        error_messages = check_user_credentials({
+            'username': request.POST.get('username'),
+            'password1': request.POST.get('password1'),
+            'password2': request.POST.get('password2')
+        })
+        context = {
+                'form': form,
+                'error_messages': error_messages
+        }
+        if len(error_messages.keys()) == 0:
+            if form.is_valid():
+                user = form.save()
+                if user is not None:
+                    return redirect('login')
+        else:
+            return render(request=request, template_name='signup.html', context=context)
+
+
+def logout_function(request):
+    logout(request=request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def index(request):
     return render(request=request, template_name='home.html')
+
+
+def check_user_credentials(user_info):
+    username = user_info['username']
+    password1 = user_info['password1']
+    password2 = user_info['password2']
+
+    all_user = User.objects.all()
+    error_messages = {}
+    for single_user in all_user:
+        if username == single_user.username:
+            error_messages['username_error_msg'] = "Username is taken"
+
+    if password1 != password2:
+        error_messages['password_error_msg'] = "Password doesn't match"
+    else:
+        if len(password1) < 8:
+            error_messages['password_error_msg'] = "Minimum password length is 8"
+    
+    return error_messages
