19392058c24ef2d2acc7c5803880f31133a7cce5
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-24 13:01:12 +0600

    html templates and css added

--- image_search/urls.py
+++ image_search/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from image_search import views
 
 urlpatterns = [
-    path('', view=views.home, name='home'),
+    path('', view=views.index, name='index'),
 ]
--- image_search/views.py
+++ image_search/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('<p>This is Home page</p>')
+def index(request):
+    return render(request=request, template_name='home.html')
