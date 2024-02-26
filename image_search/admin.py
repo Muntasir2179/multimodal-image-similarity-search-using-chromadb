cacfb91c068883dc2c59dd9305aff3a86ba0470d
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-02-26 15:08:31 +0600

    database model removed

--- image_search/admin.py
+++ image_search/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import ImageData
 
 # Register your models here.
-
-admin.site.register(ImageData)
--- image_search/models.py
+++ image_search/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class ImageData(models.Model):
-    name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/', default='')
