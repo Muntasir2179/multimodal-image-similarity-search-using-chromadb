8721179614267a037b40ae9a8d5e4a1c69bec551
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-03-06 23:24:03 +0600

    README and requirements.txt setup

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ import os
 import sys
 
 from dashboard.settings import BASE_DIR
-import shutil
 
 
 def main():
@@ -25,7 +24,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from exc    
     execute_from_command_line(sys.argv)
 
 
